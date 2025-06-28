2a8a6328124ad553e2f370c1efa17fdc764a2b09
Yash Gupta <gyash21@gmail.com>
2025-06-28 20:00:47 +0530

    add blog

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -122,7 +122,7 @@ function MobileNavigation(
         <nav className="mt-6">
           <ul className="-my-2 divide-y divide-zinc-100 text-base text-zinc-800 dark:divide-zinc-100/5 dark:text-zinc-300">
             <MobileNavItem href="/about">About</MobileNavItem>
-            <MobileNavItem href="/articles">Articles</MobileNavItem>
+            <MobileNavItem href="/articles">Blog</MobileNavItem>
             <MobileNavItem href="/projects">Projects</MobileNavItem>
           </ul>
         </nav>
@@ -165,7 +165,7 @@ function DesktopNavigation(props: React.ComponentPropsWithoutRef<'nav'>) {
     <nav {...props}>
       <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 ring-1 shadow-lg shadow-zinc-800/5 ring-zinc-900/5 backdrop-blur-sm dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
         <NavItem href="/about">About</NavItem>
-        <NavItem href="/articles">Articles</NavItem>
+        <NavItem href="/articles">Blog</NavItem>
         <NavItem href="/projects">Projects</NavItem>
       </ul>
     </nav>
